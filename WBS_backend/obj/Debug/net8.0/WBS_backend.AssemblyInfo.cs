11abdad2bd27a186bc32ee9d5ad461a37efed6c7
Viet Nhan <vietnhan033@gmail.com>
2026-01-06 15:41:07 +0700

    fix

--- WBS_backend/obj/Debug/net8.0/WBS_backend.AssemblyInfo.cs
+++ WBS_backend/obj/Debug/net8.0/WBS_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WBS_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e433774a5ee00309ac008fcb34242b22059eca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca1d57d02c931414571503ce9fdceafc5ddbfe72")]
 [assembly: System.Reflection.AssemblyProductAttribute("WBS_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WBS_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
